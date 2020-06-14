e3363a240ab271f6dee72cee8da2b4d74fa81837
Vishal <vishal.s.kumar99@gmail.com>
2020-06-15 01:40:36 +0530

    more regret

--- vue-app/src/components/Cell.vue
+++ vue-app/src/components/Cell.vue
@@ -1,11 +1,27 @@
 <template>
-    <div class="btn">
+    <div class="btn" v-bind:class="{ selected: selected }" v-on:click="select">
     </div>
 </template>
 
 <script>
 export default {
-    name: 'Cell'
+    name: 'Cell',
+    props: {
+        x: Number,
+        y: Number
+    },
+    data() {
+        return {
+            selected: false
+        }
+    },
+    methods: {
+        select() {
+            if (this.selected === false) this.selected = true;
+            else this.selected = false;
+            this.$emit('click-cell', { coords: [this.x, this.y], selected: this.selected });
+        }
+    }
 }
 </script>
 
@@ -14,8 +30,19 @@ export default {
         display: inline-block;
         border-radius: 50%;
         width: 3%;
-        height: 3%;
-        padding-top: 3%;
-        background: #ef8913;
+        height: auto;
+        padding: 3%;
+        background: #a0a0a0;
+    }
+    .btn:hover {
+        display: inline-block;
+        border-radius: 50%;
+        width: 3%;
+        height: auto;
+        padding: 3%;
+        background: #ff0000;
+    }
+    .selected {
+        background: #8b1010;
     }
 </style>
--- vue-app/src/components/Grid.vue
+++ vue-app/src/components/Grid.vue
@@ -1,9 +1,9 @@
 <template>
     <div>
         <div class="container-flex">
-            <div class="row" v-for="index in 10" :key="index">
-                <div class="column">
-                    <Cell/>
+            <div class="row p-3" v-for="x in 8" :key="x">
+                <div class="col p-1" v-for="y in 8" :key="y">
+                    <Cell v-bind:x="x" v-bind:y="y" @click-cell="clickCell"/>
                 </div>
             </div>
         </div>
@@ -12,10 +12,29 @@
 
 <script>
 import Cell from './Cell.vue';
+
 export default {
     name: 'Grid',
     components: {
         Cell
+    },
+    data() {
+        return {
+            selectedCells: []
+        }
+    },
+    methods: {
+        clickCell(data) {
+            if (data.selected === true) this.selectedCells.push(data.coords);
+            else {
+                this.selectedCells.splice(
+                    this.selectedCells.findIndex( item => 
+                        item[0] == data.coords[0] && item[1] == data.coords[1]
+                    ), 
+                1);
+            }
+            console.log(this.selectedCells);
+        }
     }
 }
 </script>
