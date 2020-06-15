aa0996ddc20da2030a0e90a17c89530dd83dd803
Vishal <vishal.s.kumar99@gmail.com>
2020-06-15 19:27:50 +0530

    make home sweeter

--- vue-app/src/views/Home.vue
+++ vue-app/src/views/Home.vue
@@ -1,14 +1,28 @@
 <template>
     <div class="container">
         <div class="row">
-            <div class="col p-3">
+            <div class="col mt-3">
                 <h1 class="display-1">Battleship</h1>
             </div>
         </div>
         <div class="row">
             <div class="col m-3">
-                <button class="btn btn-lg btn-outline-primary m-3" v-on:click="createRoom">Create room</button>
-                <button class="btn btn-lg btn-outline-primary m-3" v-on:click="joinRoom">Join room</button>
+                <button class="btn btn-lg btn-outline-primary" v-on:click="createRoom">Create room</button>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <h4>~or~</h4>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm">
+                <div class="input-group m-3">
+                    <input type="text" class="form-control w-25" placeholder="Enter room ID">
+                    <div class="input-group-append">
+                        <button class="btn btn-outline-primary" type="button" v-on:click="joinRoom">Join room</button>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
